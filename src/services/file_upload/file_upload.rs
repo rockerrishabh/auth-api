8735fc06f0f23e88f9c68af954e326485113f73f
Rishabh Kumar <rishabh.6542@gmail.com>
2025-08-29 15:00:25 +0530

    fixed several bugs

--- src/services/file_upload/file_upload.rs
+++ src/services/file_upload/file_upload.rs
@@ -33,24 +33,6 @@ impl FileUploadService {
         Ok(temp_image)
     }
 
-    /// Process and save an uploaded avatar image
-    pub async fn process_avatar(&self, payload: Multipart) -> AuthResult<ProcessedImage> {
-        // Create validator and extract file data
-        let validator = super::validator::FileValidator::new(&self.config);
-        let image_data = validator.extract_file_data(payload, "avatar").await?;
-
-        // Create image processor and generate filename
-        let image_processor = super::image_processor::ImageProcessor::new(&self.config);
-        let filename = image_processor.generate_filename(&image_data)?;
-
-        // Process and save images
-        let processed_image = image_processor
-            .process_and_save_image(&filename, &image_data)
-            .await?;
-
-        Ok(processed_image)
-    }
-
     /// Delete old avatar files
     pub async fn delete_old_avatars(
         &self,
